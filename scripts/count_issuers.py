92a32ecc6616186c6dfa37eccec6d5fb7f1360ad
Daniel Levenson <dleve123@gmail.com>
2022-01-05 13:41:58 -0500

    format/better document count_issuers.py
    - run black
    - clarify that script writes CSV to stdout

--- scripts/count_issuers.py
+++ scripts/count_issuers.py
@@ -12,30 +12,33 @@ def count_issuers(issuers_json: dict) -> int:
 
     return num_issuers
 
+
 def get_commits_changing_issuers_manifest(manifest_filepath: str) -> Iterator[Commit]:
-    repo = Repo('.')
+    repo = Repo(".")
     scoped_commits = filter(
         lambda commit: manifest_filepath in commit.stats.files.keys(),
-        repo.iter_commits()
+        repo.iter_commits(),
     )
 
     return scoped_commits
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Counts SHC issuers at input filepath')
-    parser.add_argument('input_filepath', help='JSON manifest file of issuers')
+    parser = argparse.ArgumentParser(
+        description="Counts SHC issuers at input filepath. Writes CSV of counts to stdout."
+    )
+    parser.add_argument("input_filepath", help="JSON manifest file of issuers")
     args = parser.parse_args()
 
-    commits_changing_issuers_manifest = get_commits_changing_issuers_manifest(args.input_filepath)
+    commits_changing_issuers_manifest = get_commits_changing_issuers_manifest(
+        args.input_filepath
+    )
     data = []
     for commit in commits_changing_issuers_manifest:
-        vci_issuers_at_commit = json.load(
-            commit.tree[args.input_filepath].data_stream
-        )
+        vci_issuers_at_commit = json.load(commit.tree[args.input_filepath].data_stream)
         total_num_issuers = count_issuers(vci_issuers_at_commit)
         commit_datetime = commit.committed_datetime.isoformat()
         data.append([commit_datetime, total_num_issuers])
 
-    df = pd.DataFrame(data, columns=['commit_datetime', 'total_num_issuers'])
+    df = pd.DataFrame(data, columns=["commit_datetime", "total_num_issuers"])
     print(df.to_csv(index=False))
